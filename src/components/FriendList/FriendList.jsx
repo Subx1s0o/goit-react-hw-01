f60197e83b1c492a01850a430ff11144c1d8fa96
Subx1s0o <nazarsabardin@gmail.com>
2024-08-23 17:08:47 +0300

    .

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,4 +1,4 @@
-import FriendListItem from "./FriendListItem";
+import FriendListItem from "../FriendListItem/FriendListItem";
 import css from "./FriendListItem.module.css";
 export default function FriendList({ friends }) {
   return (
