19568708eee3650996dfbb01219af957a31017fb
Subx1s0o <nazarsabardin@gmail.com>
2024-08-23 17:43:36 +0300

    .

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -2,12 +2,14 @@ import css from "./FriendListItem.module.css";
 
 export default function FriendListItem({ avatar, name, isOnline }) {
   return (
-    <div className={css.friendItem}>
-      <img src={avatar} alt="Avatar" width="48" />
-      <p>{name}</p>
-      <p className={isOnline ? css.online : css.offline}>
-        {isOnline ? "Online" : "Offline"}
-      </p>
-    </div>
+    <li className={css.friendsLi}>
+      <div className={css.friendItem}>
+        <img src={avatar} alt="Avatar" width="48" />
+        <p>{name}</p>
+        <p className={isOnline ? css.online : css.offline}>
+          {isOnline ? "Online" : "Offline"}
+        </p>
+      </div>
+    </li>
   );
 }
