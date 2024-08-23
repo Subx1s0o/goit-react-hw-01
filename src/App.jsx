4f786ee84be35f712ddf7cdcd2c1bc3b1ae3230c
Subx1s0o <nazarsabardin@gmail.com>
2024-08-23 16:20:48 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
+import FriendList from "./components/FriendList/FriendList";
+import Profile from "./components/Profile/Profile";
+import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
 import "./css/App.css";
-import FriendList from "./Components/FriendList/FriendList";
-import Profile from "./Components/Profile/Profile";
-import TransactionHistory from "./Components/TransactionHistory/TransactionHistory";
-import userData from "./userData.json";
 import friends from "./friends.json";
 import transactions from "./transactions.json";
+import userData from "./userData.json";
 function App() {
   return (
     <div className="container">
